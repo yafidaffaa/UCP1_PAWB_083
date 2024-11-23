fe75e147781e651d4125240b78f1a2fce4308813
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-11-23 15:10:16 +0700

    menambahkan variabel yang berisi jalur untuk ke controller bibit

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 const home = require("./controller-home");
 const pupuk = require("./controller-pupuk");
+const bibit = require("./controller-bibit");
 
 module.exports = {
     home,
     pupuk,
+    bibit
 };
